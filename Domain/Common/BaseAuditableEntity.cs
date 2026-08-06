547536f5256c2cd884d9686c00469153c35cbc06
Antonio Trajkovski <antonio.trajkovski.1@students.finki.ukim.mk>
2026-08-06 12:07:18 +0200

    First commit BaseEntity.cs and BaseAuditableEntity.cs

---
+++ Domain/Common/BaseAuditableEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Common;
+
+public abstract class BaseAuditableEntity<TU> : BaseEntity
+{
+    public string CreatedById { get; set; }
+    public DateTime DateCreated { get; set; }
+
+    public string LastModifiedById { get; set; }
+    public DateTime? DateLastModified { get; set; }
+}
